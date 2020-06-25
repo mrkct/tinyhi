fdc86fa6ba580c1f1ffe9c7fc72c8e4f32385a52
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2020-06-25 10:22:32 +0200

    Interpreter: Added tests for assignment and operators

--- tests/interpreter/test_interpreter.py
+++ tests/interpreter/test_interpreter.py
@@ -2,7 +2,7 @@ import pytest
 from tinyhi.interpreter import run, ExecutionError
 
 
-def test_return():
+def test_main_return():
     source = r"""BEGIN main
         main <- 123
     END"""
@@ -11,4 +11,52 @@ def test_return():
         main <- 1 + 4 * 3
     END"""
     assert run(source) == 13
-    
+
+def test_assignment():
+    source = r"""BEGIN main
+        x <- 1
+        main <- x
+    END"""
+    assert run(source) == 1
+    source = r"""BEGIN main
+        x <- 1
+        x <- x + 1
+        main <- x
+    END"""
+    assert run(source) == 2
+
+def test_lists():
+    source = r"""BEGIN main
+        x <- 1 2 3
+        y <- x 4
+        main <- y
+    END"""
+    assert run(source) == [1, 2, 3, 4]
+    source = r"""BEGIN main
+        x <- 1 2
+        y <- 3 4
+        main <- 0 x y 5
+    END"""
+    assert run(source) == [0, 1, 2, 3, 4, 5]
+
+def test_operations():
+    source = r"""BEGIN main
+        main <- 2 {} 1 2 3
+    END"""
+    expected = [
+        ('+', [3, 4, 5]), 
+        ('-', [1, 0, -1]), 
+        ('*', [2, 4, 6]), 
+        ('/', [2, 1, 0]), 
+        (' ', [2, 1, 2, 3])
+    ]
+    for op, expect in expected:
+        assert run(source.format(op)) == expect
+    source = r"""BEGIN main
+        main <- ~ 1 2 3
+    END"""
+    assert run(source) == [-1, -2, -3]
+    source = r"""BEGIN main
+        main <- # 1 2 3
+    END"""
+    assert run(source) == 3

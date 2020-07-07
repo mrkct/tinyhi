6ff6ff291c660263caed3ae00f9d0d17f0d66fcc
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2020-07-07 09:36:12 +0200

    Changed tests to use mark.parametrize

--- tests/test_parse_programs.py
+++ tests/test_parse_programs.py
@@ -6,18 +6,19 @@ import pytest
 CORRECT_PROGRAMS_FOLDER = 'tests/programs/correct/'
 INCORRECT_PROGRAMS_FOLDER = 'tests/programs/incorrect/'
 
-def test_correct():
-    for filename in os.listdir(CORRECT_PROGRAMS_FOLDER):
-        with open(CORRECT_PROGRAMS_FOLDER + filename, 'r') as file:
-            source = file.read()
-        result = parse(source, throw_errors=True)
-        print(filename, '...')
-        assert result != None
+CORRECT_PROGRAMS = os.listdir(CORRECT_PROGRAMS_FOLDER)
+INCORRECT_PROGRAMS = os.listdir(INCORRECT_PROGRAMS_FOLDER)
 
-def test_incorrect():
-    for filename in os.listdir(INCORRECT_PROGRAMS_FOLDER):
-        with open(INCORRECT_PROGRAMS_FOLDER + filename, 'r') as file:
-            source = file.read()
-        with pytest.raises(ParseError) as info:
-            print(filename, '...')
-            result = parse(source, throw_errors=True)
+@pytest.mark.parametrize('filename', CORRECT_PROGRAMS)
+def test_correct(filename):
+    with open(CORRECT_PROGRAMS_FOLDER + filename, 'r') as file:
+        source = file.read()
+    result = parse(source, throw_errors=True)
+    assert result != None
+
+@pytest.mark.parametrize('filename', INCORRECT_PROGRAMS)
+def test_incorrect(filename):
+    with open(INCORRECT_PROGRAMS_FOLDER + filename, 'r') as file:
+        source = file.read()
+    with pytest.raises(ParseError) as info:
+        result = parse(source, throw_errors=True)
